e06e8d0360284e8593034292a633b859276f096c
David Lopes <davidllopes@gmail.com>
2022-02-13 12:40:03 +0000

    --add style to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,22 @@
-const Header = () => {
-    return <div className="Header"></div>;
+import styled from "styled-components";
+
+const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
+`;
+
+const HeaderComponent = () => {
+    return (
+        <Header className="Header">
+            <div>
+                <h1 className="jumbo-title">David Lopes</h1>
+                <h2>Portfolio</h2>
+            </div>
+        </Header>
+    );
 };
 
-export default Header;
+export default HeaderComponent;
