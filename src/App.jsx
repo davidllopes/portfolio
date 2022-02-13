191181011d7dc34e91fb5a4b4f81a39dcc033d82
David Lopes <davidllopes@gmail.com>
2022-02-13 17:39:03 +0000

    --added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import HeaderComponent from "./components/Header";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import HeaderComponent from "./components/Header/Header";
+import LoadingAnim from "./components/Loader/Loader";
+import RecentWork from "./components/RecentWork/RecentWork.jsx";
 
+const AppContainer = styled.div`
+    opacity: 0;
+    transition: opacity 1s ease-in-out;
+`;
 function App() {
+    const [loaderStyle, setLoaderStyle] = useState({});
+    const [AppContStyle, setAppContStyle] = useState({});
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoaderStyle({ opacity: 0 });
+            setAppContStyle({ opacity: 1 });
+        }, 2500);
+    }, []);
+
     return (
         <div className="App">
-            <HeaderComponent />
+            <LoadingAnim style={loaderStyle} />
+            <AppContainer style={AppContStyle}>
+                <HeaderComponent />
+                <RecentWork></RecentWork>
+            </AppContainer>
         </div>
     );
 }
