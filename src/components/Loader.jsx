d82be4016b60ac924fa0e3786cd5a8b2f5c761c6
David Lopes <davidllopes@gmail.com>
2022-02-13 12:15:32 +0000

    --first commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+const LoadingAnim = () => {
+    return (
+        <div>
+            <div className="loader loader1"></div>
+            <div className="loader loader2"></div>
+            <div className="loader loader3"></div>
+            <div className="loader loader4"></div>
+        </div>
+    );
+};
+
+export default LoadingAnim;
