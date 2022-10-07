8681f25cc443ae503ce32f62a6c28a8c0ee3f1e7
David Lopes <david.lopes@avadolearning.com>
2022-10-07 17:31:42 +0100

    upload avado way cipd

--- src/parts/Header/Header.jsx
+++ src/parts/Header/Header.jsx
@@ -1,36 +1,29 @@
 import {
-    Header,
-    HeaderOverlay,
-    HeaderVideo,
-    HeaderContainer,
+   Header,
+   HeaderOverlay,
+   HeaderVideo,
+   HeaderContainer,
 } from "./Header.styled";
 
 const HeaderComponent = () => {
-    return (
-        <HeaderContainer>
-            <Header className="Header">
-                <HeaderVideo
-                    autoPlay={true}
-                    muted
-                    loop
-                    className="header__video"
-                >
-                    <source
-                        src="static/assets/videos/mountains_-_72914.mp4"
-                        type="video/mp4"
-                    />
-                </HeaderVideo>
-                <HeaderOverlay>
-                    <div>
-                        <h1 className="heading heading--jumbo">David Lopes</h1>
-                        <h4 className="subheading">
-                            Web Developer &bull; Designer
-                        </h4>
-                    </div>
-                </HeaderOverlay>
-            </Header>
-        </HeaderContainer>
-    );
+   return (
+      <HeaderContainer>
+         <Header className="Header">
+            <HeaderVideo autoPlay={true} muted loop className="header__video">
+               <source
+                  src="static/assets/videos/mountains_-_72914.mp4"
+                  type="video/mp4"
+               />
+            </HeaderVideo>
+            <HeaderOverlay>
+               <div>
+                  <h1 className="heading heading--jumbo">David Lopes</h1>
+                  <h4 className="subheading">Creative Developer</h4>
+               </div>
+            </HeaderOverlay>
+         </Header>
+      </HeaderContainer>
+   );
 };
 
 export default HeaderComponent;
