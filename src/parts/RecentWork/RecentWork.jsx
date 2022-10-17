821b939978fa16a0c6fb7b5f4b58bfaf26040a09
David Lopes <david.lopes@avadolearning.com>
2022-10-17 16:52:15 +0100

    colour fixes

--- src/parts/RecentWork/RecentWork.jsx
+++ src/parts/RecentWork/RecentWork.jsx
@@ -10,20 +10,22 @@ const RecentWork = () => {
       <Card className="recent" content={item} key={item.title}></Card>
    ));
    return (
-      <section className="section recent-work">
-         <div className="full-width">
-            <h2 className="heading heading--secondary section__title">
-               Recent Work
-            </h2>
-         </div>
-         <div className={`card card-recent card--heading`}>
-            <div className="container">
-               <CardThumbnail content={data.headExample[0]} />
-               <CardText content={data.headExample[0]} />
+      <div>
+         <section className="section recent-work">
+            <div className="full-width">
+               <h2 className="heading heading--secondary section__title">
+                  Recent Work
+               </h2>
             </div>
-         </div>
-         {list}
-      </section>
+            <div className={`card--heading`}>
+               <div className="container">
+                  <CardThumbnail content={data.headExample[0]} />
+                  <CardText content={data.headExample[0]} />
+               </div>
+            </div>
+         </section>
+         <section className="section section--alt">{list}</section>
+      </div>
    );
 };
 
