4b0d18bd69d370a567bb50c0f43eafe6cf88778c
David Lopes <david.lopes@avadolearning.com>
2022-10-17 15:31:44 +0100

    header card fix

--- src/parts/RecentWork/RecentWork.jsx
+++ src/parts/RecentWork/RecentWork.jsx
@@ -1,4 +1,6 @@
 import Card from "../../components/Card/Card";
+import CardText from "../../components/Card/CardText";
+import CardThumbnail from "../../components/Card/CardThumbnail";
 import data from "./RecentWork.json";
 import "./_recent-work.scss";
 
@@ -9,11 +11,16 @@ const RecentWork = () => {
    ));
    return (
       <section className="section recent-work">
-         <Card className="recent" content={data.workExamples[0]}></Card>
          <div className="full-width">
             <h2 className="heading heading--secondary section__title">
                Recent Work
             </h2>
+         </div>
+         <div className={`card card-recent card--heading`}>
+            <div className="container">
+               <CardThumbnail content={data.headExample[0]} />
+               <CardText content={data.headExample[0]} />
+            </div>
          </div>
          {list}
       </section>
