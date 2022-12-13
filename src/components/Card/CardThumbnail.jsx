7cce72386cd37c4200140144c8e60a198176dbd7
David Lopes <davidllopes@gmail.com>
2022-12-13 18:19:37 +0000

    react fix for CardThumbnail

--- src/components/Card/CardThumbnail.jsx
+++ src/components/Card/CardThumbnail.jsx
@@ -1,23 +1,25 @@
-export default function CardThumbnail({ content, className }) {
-   const thumbnailModifier = content.bigThumbnail
-      ? " card__thumbnail--big"
-      : "";
-   const thumb2x = content.thumbnail2x
-      ? ", " + content.imgsPath + content.thumbnail2x + " 2x"
-      : "";
-   if (content.thumbnail) {
-      return (
-         <div
-            className={`card__thumbnail ${thumbnailModifier} ${
-               className ? className : ""
-            }`}
-         >
-            <img
-               srcSet={`${content.imgsPath}${content.thumbnail}${thumb2x}`}
-               className="img-responsive"
-               alt=""
-            />
-         </div>
-      );
-   }
-}
+const CardThumbnail = ({ content, className }) => {
+    const thumbnailModifier = content.bigThumbnail
+        ? " card__thumbnail--big"
+        : "";
+    const thumb2x = content.thumbnail2x
+        ? ", " + content.imgsPath + content.thumbnail2x + " 2x"
+        : "";
+    if (content.thumbnail) {
+        return (
+            <div
+                className={`card__thumbnail ${thumbnailModifier} ${
+                    className ? className : ""
+                }`}
+            >
+                <img
+                    srcSet={`${content.imgsPath}${content.thumbnail}${thumb2x}`}
+                    className="img-responsive"
+                    alt=""
+                />
+            </div>
+        );
+    }
+};
+
+export default CardThumbnail;
